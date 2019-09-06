a3e9a79bc4ef9478140625af252d206ab33869f1
Joonas Taivalmaa <Joonas.Taivalmaa@metropolia.fi>
2019-09-06 15:39:18 +0300

    added MapModel

--- FightPub2/src/main/java/Model/MapModel.java
+++ FightPub2/src/main/java/Model/MapModel.java
@@ -25,23 +25,14 @@ import java.util.Arrays;
  * @author Pate
  */
 public class MapModel {
-    
-    public MapModel () {
-        
-    HashMap<String, List<Integer>> mapLines = new HashMap<>();
-    
-    // (X1, Y1, X2, Y2)
-    final Integer[] bottomOfMap = { 0, 0, 3000, 0};
-    final Integer[] rightOfMap = {3000, 0, 3000, 1920};
-    final Integer[] topOfMap = {3000, 1920, 0, 1920};
-    final Integer[] leftOfMap = {0, 1920, 0, 0};
-    
-    mapLines.put("bottom", Arrays.asList(bottomOfMap));
-    mapLines.put("right", Arrays.asList(rightOfMap));
-    mapLines.put("top", Arrays.asList(topOfMap));
-    mapLines.put("left", Arrays.asList(leftOfMap));
-    
-    
-    }
+
+        final int LEFTBORDER = 0;
+        final int RIGHTBORDER = 3000;
+        final int BOTTOM = 0;
+
+        private string name;
+
+    public MapModel (String name) {
+        this.name = name;
 
 }
